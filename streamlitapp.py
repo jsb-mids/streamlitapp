c94000fa8a01d389a6d557dc264c2b5869706041
jsb-mids <jujhar.bedi@berkeley.edu>
2023-12-04 16:13:54 -0800

    ui updates

--- streamlitapp.py
+++ streamlitapp.py
@@ -99,7 +99,7 @@ for idx, message in enumerate(st.session_state.messages):
 
 # If the last message is not from the assistant, generate a new response
 if st.session_state.messages[-1]["role"] == "user":
-    with st.chat_message("assistant", avatar=st.image('logos/Chatbot Logo.png')):
+    with st.chat_message("assistant", avatar=st.image('./logos/Chatbot Logo.png')):
         with st.spinner("Thinking..."):
             response = assistant_response["content"]  # Get the assistant's response from the dictionary
             st.write(response)
